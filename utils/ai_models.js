6687d0ac629b2fc5022a78e7730a71ef494dceb8
juber-nowawave <juber.nowawave@gmail.com>
2026-03-06 16:58:57 +0530

    changes in stock-analysis api format

--- utils/ai_models.js
+++ utils/ai_models.js
@@ -1,28 +1,39 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatBedrockConverse } from "@langchain/aws";
+
 const modelInstances = new Map();
 
 export const getModelInstance = (apiKey) => {
   if (!modelInstances.has(apiKey)) {
-    modelInstances.set(apiKey, new ChatGoogleGenerativeAI({
-      model: process.env.GOOGLE_MODEL || "gemini-2.5-flash",
-      apiKey: apiKey,
-      temperature: 0.1,
-    }));
+    modelInstances.set(
+      apiKey,
+      new ChatGoogleGenerativeAI({
+        model: process.env.GOOGLE_MODEL || "gemini-2.5-flash",
+        apiKey: apiKey,
+        temperature: 0.1,
+      })
+    );
   }
   return modelInstances.get(apiKey);
 };
 
 export const getBedrockLangChainInstance = (apiKey) => {
-  if (!modelInstances.has(`langchain_${apiKey}`)) {
-    modelInstances.set(`langchain_${apiKey}`, new ChatOpenAI({
-      model: process.env.OPENAI_MODEL || "openai.gpt-oss-20b-1:0",
-      apiKey: apiKey,
-      configuration: {
-        baseURL: process.env.BEDROCK_ENDPOINT,
-      },
-      temperature: 0.1,
-    }));
+  const key = `bedrock_${apiKey}`;
+
+  if (!modelInstances.has(key)) {
+    modelInstances.set(
+      key,
+      new ChatBedrockConverse({
+        model: process.env.OPENAI_MODEL || "openai.gpt-oss-20b-1:0",
+        region: process.env.AWS_REGION || "us-east-1",
+        temperature: 0.1,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+      })
+    );
   }
-  return modelInstances.get(`langchain_${apiKey}`);
+
+  return modelInstances.get(key);
 };
